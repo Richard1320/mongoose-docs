0d8654be9f7e6584eb55f376ec7cde5be08a5a9d
Richard <richardhung111@gmail.com>
2021-06-12 14:49:50 -0700

    Typescript interfaces

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+import {Schema} from "mongoose";
+
+export type TMongooseDocsFieldsObject = { [fieldName: string] : IMongooseDocsSchemaField };
+
+export interface IMongooseDocsSchemaField {
+	type: string;
+	required: boolean;
+	comment?: string;
+	default?: string;
+	min?: number | Schema.Types.Date | [number, string] | [Schema.Types.Date, string] | readonly [number, string] | readonly [Schema.Types.Date, string];
+	max?: number | Schema.Types.Date | [number, string] | [Schema.Types.Date, string] | readonly [number, string] | readonly [Schema.Types.Date, string];
+	nestedSchema?: TMongooseDocsFieldsObject | string;
+}
+
+export interface IMongooseDocsSchema {
+	name: string;
+	fields: TMongooseDocsFieldsObject;
+}
+

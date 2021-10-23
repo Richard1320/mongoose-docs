77e2d2b351e7d4e2e9df3d20e4757c970df7ae07
Richard <richardhung111@gmail.com>
2021-10-23 16:54:44 -0700

    Test connection with createConnection()

--- test/test.ts
+++ test/test.ts
@@ -1,17 +1,35 @@
-import * as mongoose from "mongoose";
+import  mongoose, {Connection} from "mongoose";
 import {blogSchema, categorySchema} from "./schema";
 import {expect} from "chai";
 import {mongooseDocsJSON} from "../dist";
 import comparisonJSON from "./comparison.json";
 import {IMongooseDocsSchema} from "../types";
 
-describe('Mongoose Docs', () => {
+describe('Mongoose Instance Docs', () => {
 	// Implement mongoose models
 	mongoose.model('Category', categorySchema);
 	mongoose.model('Blog', blogSchema);
 
 	// Pass in the Mongoose instance with the models implemented.
 	const schemaStructure:IMongooseDocsSchema[] = mongooseDocsJSON(mongoose);
+
+	// Properties with undefined values are actually still set. Need to stringify and re-parse the data.
+	const schemaJSON:IMongooseDocsSchema[] = JSON.parse(JSON.stringify(schemaStructure));
+
+	it('should match the comparison JSON file', (done) => {
+		expect(schemaJSON).to.eql(comparisonJSON);
+		done();
+	});
+
+});
+describe('Mongoose Connection Docs', () => {
+	const mongooseConnection: Connection =  mongoose.createConnection();
+	// Implement mongoose models
+	mongooseConnection.model('Category', categorySchema);
+	mongooseConnection.model('Blog', blogSchema);
+
+	// Pass in the Mongoose instance with the models implemented.
+	const schemaStructure:IMongooseDocsSchema[] = mongooseDocsJSON(mongooseConnection);
 
 	// Properties with undefined values are actually still set. Need to stringify and re-parse the data.
 	const schemaJSON:IMongooseDocsSchema[] = JSON.parse(JSON.stringify(schemaStructure));
